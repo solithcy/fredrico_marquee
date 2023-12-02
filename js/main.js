7e2d85bc00a4fdbdea41e1e19390ae44d1139dc1
Federico Slongo <federicoslongo@gmail.com>
2023-12-02 11:03:03 +0100

    Warning for mobile users and stoping text :)

---
+++ js/main.js
@@ -0,0 +1,5 @@
+function startup() {
+    moveTitle(); // Start the animation
+
+    if (navigator.userAgent.toLowerCase().match(/mobile/i)) { alert("This site was designed to be viewed from a computer, the experience on mobile isn't good"); }
+}
--- js/movingText.js
+++ js/movingText.js
@@ -1,6 +1,6 @@
 var per = 0;
 var sleep = 300;
-var direction = false;
+var direction = false, mouseOv = false;
 
 function moveTitle() {
     setTimeout(function() {
@@ -14,11 +14,16 @@ function moveTitle() {
             direction = true;
         if(per < -40)
             direction = false;
-
-        moveTitle(); // Call the function again for the next step
+        if(!mouseOv)
+            moveTitle(); // Call the function again for the next step
     }, sleep);
 }
 
-function startup() {
-    moveTitle(); // Start the animation
+function stopMove(){
+    mouseOv = true;
+}
+
+function reStartMove(){
+    mouseOv = false;
+    moveTitle();
 }
