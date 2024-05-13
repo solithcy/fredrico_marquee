87450b7deeffc60230a822af0266a63b264e6860
Federico <federicoslongo@gmail.com>
2024-05-13 17:38:52 +0000

    Changed some stuff :)

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ function startup() {
 
     // Checks if the person on the site is a mobiles user and is so it alerts them that the site is not good on mobile
     if (navigator.userAgent.toLowerCase().match(/mobile/i)) { alert("This site was designed to be viewed from a computer, the experience on mobile isn't good"); }
-    else { alert("The site is best enjoyed at 90 % browser scale") }
 }
 
 //Calculates my age
