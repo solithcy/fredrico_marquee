6a9c841f8d3dc7a47478a732344c6c31e4c2b31f
Federico <federicoslongo@gmail.com>
2024-12-20 22:14:37 +0100

    Added playing now throug last fm

---
+++ homePage/js/lastfm.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    function fetchNowPlaying() {
+        $.ajax({
+            url: '/now-playing',
+            type: 'GET',
+            dataType: 'json',
+            success: function(data) {
+                var nowPlayingText = data.nowPlaying === 'No music currently playing' ? data.nowPlaying : `<a href="${data.url}" target="_blank">${data.nowPlaying}</a>`;
+                $('#now-playing').html(nowPlayingText);
+            },
+            error: function(error) {
+                console.log('Error:', error);
+            }
+        });
+    }
+
+    fetchNowPlaying();
+    setInterval(fetchNowPlaying, 10000); // Fetch every 10 seconds
+});
---
+++ homePage/serverSide.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = 3000;
+
+const apiKey = 'YOUR_API_KEY';
+const user = 'TheCatItalyDude';
+
+app.use(express.static('public'));
+
+app.get('/now-playing', async (req, res) => {
+    try {
+        const response = await axios.get(`https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${user}&api_key=${apiKey}&format=json`);
+        const track = response.data.recenttracks.track[0];
+        const nowPlaying = track["@attr"] && track["@attr"].nowplaying ? `${track.artist['#text']} - ${track.name}` : 'No music currently playing';
+        res.json({ nowPlaying: nowPlaying, url: track.url });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
